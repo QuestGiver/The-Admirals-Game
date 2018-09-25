0af9d6997abd21f11442bcc13aa0edbd347f2ed8
QuestGiver <cpt.gastard@gmail.com>
2018-09-24 17:58:58 -0700

    add a lot

---
+++ Assets/Scripts/CameraControls/CameraController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+    public float mouseSensativity = 100;
+    Camera cam;
+
+
+    bool leftClickInstance = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main;
+    }
+    Vector3 startPos = Vector3.zero;
+    Vector3 endPos = Vector3.zero;
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        //rb.AddRelativeTorque(transform.up * Input.GetAxisRaw("Mouse X") * mouseSensativity * Time.deltaTime);
+
+
+        //rb.AddRelativeTorque(transform.right * Input.GetAxisRaw("Mouse Y") * -mouseSensativity * Time.deltaTime);
+
+        //rb.AddRelativeTorque(Vector3.forward * (0 - transform.rotation.z) * 1000 * Time.deltaTime);
+        //transform.Rotate(Vector3.up, Input.GetAxisRaw("Mouse X") * mouseSensativity * Time.deltaTime, Space.World);
+        //transform.Rotate(Vector3.right, Input.GetAxisRaw("Mouse Y") * -mouseSensativity * Time.deltaTime, Space.World);
+
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rb.AddForce(Vector3.down * speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            rb.AddForce(Vector3.up * speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * speed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                rb.AddForce(Vector3.forward * (speed * 2) * Time.deltaTime);
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.right * -speed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                rb.AddForce(Vector3.right * -(speed * 2) * Time.deltaTime);
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.forward * -speed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                rb.AddForce(Vector3.forward * -(speed * 2) * Time.deltaTime);
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * speed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                rb.AddForce(Vector3.right * (speed * 2) * Time.deltaTime);
+            }
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (leftClickInstance == false)
+            {
+                startPos = Input.mousePosition;
+                Debug.Log(Input.mousePosition);
+                leftClickInstance = true;
+            }
+            
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPos = Input.mousePosition;
+            rb.AddForce((startPos - endPos).x *speed* Time.deltaTime, 0, (startPos - endPos).y * speed * Time.deltaTime);
+            Debug.Log(Input.mousePosition);
+            leftClickInstance = false;
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponParticles/TriggerModuleWrapper.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerModuleWrapper : MonoBehaviour
+{
+
+    private ParticleSystem ps;
+
+    [SerializeField]
+    public Collider[] interactables;
+
+    public bool enter;
+    public bool exit;
+    public bool inside;
+    public bool outside;
+
+    public ParticleSystemOverlapAction enterAction;
+    public ParticleSystemOverlapAction exitAction;
+    public ParticleSystemOverlapAction insideAction;
+    public ParticleSystemOverlapAction outsideAction;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+
+        var shape = ps.shape;
+        shape.enabled = false;
+
+        var trigger = ps.trigger;
+        trigger.enabled = true;
+
+        for (int i = 0; i < interactables.Length; i++)
+        {
+            trigger.SetCollider(i, interactables[i]);
+        }
+    }
+
+    void Update()
+    {
+        var trigger = ps.trigger;
+        trigger.enter = enter ? enterAction : ParticleSystemOverlapAction.Ignore;
+        trigger.exit = exit ? exitAction : ParticleSystemOverlapAction.Ignore;
+        trigger.inside = inside ? insideAction : ParticleSystemOverlapAction.Ignore;
+        trigger.outside = outside ? outsideAction : ParticleSystemOverlapAction.Ignore;
+    }
+
+    void OnGUI()
+    {
+        enter = GUI.Toggle(new Rect(25, 40, 200, 30), enter, "Enter Callback");
+        exit = GUI.Toggle(new Rect(25, 80, 200, 30), exit, "Exit Callback");
+        inside = GUI.Toggle(new Rect(25, 120, 200, 30), inside, "Inside Callback");
+        outside = GUI.Toggle(new Rect(25, 160, 200, 30), outside, "Outside Callback");
+    }
+    
+    //void OnParticleTrigger()
+    //{
+    //    if (enter)
+    //    {
+    //        List<ParticleSystem.Particle> enterList = new List<ParticleSystem.Particle>();
+    //        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enterList);
+
+    //        for (int i = 0; i < numEnter; i++)
+    //        {
+    //            ParticleSystem.Particle p = enterList[i];
+
+    //            //behavior goes here
+
+
+
+    //            //behavior ends
+    //            enterList[i] = p;
+    //        }
+
+    //        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enterList);
+    //    }
+
+    //    if (exit)
+    //    {
+    //        List<ParticleSystem.Particle> exitList = new List<ParticleSystem.Particle>();
+    //        int numExit = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, exitList);
+
+    //        for (int i = 0; i < numExit; i++)
+    //        {
+    //            ParticleSystem.Particle p = exitList[i];
+    //            //behavior goes here
+
+
+
+    //            //behavior ends
+    //            exitList[i] = p;
+    //        }
+
+    //        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exitList);
+    //    }
+
+    //    if (inside)
+    //    {
+    //        List<ParticleSystem.Particle> insideList = new List<ParticleSystem.Particle>();
+    //        int numInside = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Inside, insideList);
+
+    //        for (int i = 0; i < numInside; i++)
+    //        {
+    //            ParticleSystem.Particle p = insideList[i];
+    //            //behavior goes here
+
+
+
+    //            //behavior ends
+    //            insideList[i] = p;
+    //        }
+
+    //        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Inside, insideList);
+    //    }
+
+    //    if (outside)
+    //    {
+    //        List<ParticleSystem.Particle> outsideList = new List<ParticleSystem.Particle>();
+    //        int numOutside = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Outside, outsideList);
+
+    //        for (int i = 0; i < numOutside; i++)
+    //        {
+    //            ParticleSystem.Particle p = outsideList[i];
+    //            //behavior goes here
+
+
+
+    //            //behavior ends
+    //            outsideList[i] = p;
+    //        }
+
+    //        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Outside, outsideList);
+    //    }
+    //}
+
+}
